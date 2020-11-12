609ec0a22b2b655c0945142191b22dd56ec02740
Gianluca Gippetto <gianluca.gippetto@gmail.com>
2020-11-12 17:40:11 +0100

    [src] Define a CLI for download_reports.py
    - Output the list of new reports (if any)
    - Use logging module

--- src/download_reports.py
+++ src/download_reports.py
@@ -2,9 +2,10 @@
 Parse the ISS news page to obtain all links to reports published until now,
 then download all reports missing in the ISS_REPORT folder.
 """
+import json
+import logging
 import re
 from pathlib import Path
-from pprint import pprint
 from typing import Dict
 from urllib.parse import unquote, urljoin
 
@@ -14,8 +15,9 @@ from urllib3 import Retry
 
 from common import (
     REPORTS_DIR,
-    get_report_path,
     get_italian_date_pattern,
+    get_latest_data_date,
+    get_report_path,
     process_datetime_tokens,
 )
 
@@ -32,6 +34,8 @@ EXTRA_REPORT_URLS = {
 
 # Used for extracting the report date from the pdf filename
 FILENAME_DATE_PATTERN = re.compile(get_italian_date_pattern(sep="[-_ ]"), re.IGNORECASE)
+
+logger = logging.getLogger(__name__)
 
 
 def get_date_from_report_url(fname: str) -> str:
@@ -89,14 +93,13 @@ def download_missing_reports(
     `SKIP_REPORTS_TO_DATE` are ignored.
     """
     after = max(after, SKIP_REPORTS_TO_DATE)
+    logger.info('Looking for reports following %s', after)
     Path(output_dir).mkdir(parents=True, exist_ok=True)
     session = get_http_session()
     fetched_urls = extract_report_urls_from(scrape_url, session) if scrape_url else []
     date2url = {get_date_from_report_url(url): url for url in fetched_urls}
     date2url.update(urls_by_date)
-    print("Report URLs by date:")
-    pprint(date2url, indent=2)
-    print("")
+    logger.debug("Report URLs by date:\n%s", json.dumps(date2url, indent=2))
 
     new_report_paths = []
     for date, url in date2url.items():
@@ -104,15 +107,38 @@ def download_missing_reports(
             continue
         path = get_report_path(date=date, dirpath=output_dir)
         if not path.exists():
-            print("Found new report (%s): %s" % (date, url))
-            print("Downloading to", path, "...", end=" ")
+            logger.info("Downloading new report (%s): %s", date, url)
             download_file(url, path, session)
             new_report_paths.append(path)
-            print("DONE")
     if not new_report_paths:
-        print("No new reports found")
+        logger.info("Nothing new")
     return new_report_paths
 
 
 if __name__ == "__main__":
-    download_missing_reports()
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Download PDF reports not already available in the reports/ folder. "
+                    "Outputs the (space-separated) list of new reports (if any)."
+    )
+    parser.add_argument(
+        # This option exists for running the script in GitHub actions. The repo doesn't
+        # store the PDF reports, so I use the date of the latest dataset in data/by-date
+        # to decide what report(s) to download.
+        '--since-latest-dataset-date', action='store_true',
+        help="Ignore reports predating the date of the latest dataset (in data/by-date)"
+    )
+    parser.add_argument('--debug', action="store_true")
+    args = parser.parse_args()
+
+    logging.basicConfig(
+        level=logging.DEBUG if args.debug else logging.INFO,
+        format="%(levelname)s: %(message)s"
+    )
+
+    new_paths = download_missing_reports(
+        after=get_latest_data_date() if args.since_latest_dataset_date else ''
+    )
+
+    print(" ".join(str(path) for path in new_paths))
